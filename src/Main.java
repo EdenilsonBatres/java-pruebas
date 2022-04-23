c457e3b3852d86245e33af78dde934ac02d32534
Edenilson <edenilson.batres@gmail.com>
2022-04-22 21:20:02 -0600

    funciones

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello world!");
         System.out.println("hola mundillo");
+        int resultado;
+        resultado = suma(10, 30);
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a , int b)
+    {
+        return a + b;
     }
 }
